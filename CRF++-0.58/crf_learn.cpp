7f32e4c92539f833e1f542bec5cbfbde8bd3a005
yanghh0 <13588036019@163.com>
2021-02-20 16:55:33 +0800

    Create crf_learn.cpp

---
+++ CRF++-0.58/crf_learn.cpp
@@ -0,0 +1,12 @@
+//
+//  CRF++ -- Yet Another CRF toolkit
+//  Copyright(C) 2005-2007 Taku Kudo <taku@chasen.org>
+//
+
+#include "crfpp.h"
+#include "winmain.h"
+
+int main(int argc, char **argv) 
+{
+    return crfpp_learn(argc, argv);
+}
