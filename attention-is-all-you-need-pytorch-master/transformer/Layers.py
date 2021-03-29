05f8f17a1bd0570a0bba60f737560926bc36c7cf
yanghh0 <13588036019@163.com>
2021-03-29 20:40:58 +0800

    Update Layers.py

--- attention-is-all-you-need-pytorch-master/transformer/Layers.py
+++ attention-is-all-you-need-pytorch-master/transformer/Layers.py
@@ -12,10 +12,13 @@ class EncoderLayer(nn.Module):
 
     def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
         super(EncoderLayer, self).__init__()
+        # 多头注意力机制
         self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        # 前馈全连接层
         self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
 
     def forward(self, enc_input, slf_attn_mask=None):
+        # 生成 Q K V 三个矩阵的输入相同，均为enc_input
         enc_output, enc_slf_attn = self.slf_attn(enc_input, enc_input, enc_input, mask=slf_attn_mask)
         enc_output = self.pos_ffn(enc_output)
         return enc_output, enc_slf_attn
@@ -26,12 +29,17 @@ class DecoderLayer(nn.Module):
 
     def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
         super(DecoderLayer, self).__init__()
+        # 自注意力机制
         self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        # 注意力机制
         self.enc_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        # 前馈全连接层
         self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
 
     def forward(self, dec_input, enc_output, slf_attn_mask=None, dec_enc_attn_mask=None):
+        # 自注意力机制产生 Q K V 的输入相同，均为dec_input
         dec_output, dec_slf_attn = self.slf_attn(dec_input, dec_input, dec_input, mask=slf_attn_mask)
+        # 注意力机制产生 K V 的输入相同，均为env_input，产生 Q 的输入为dec_output
         dec_output, dec_enc_attn = self.enc_attn(dec_output, enc_output, enc_output, mask=dec_enc_attn_mask)
         dec_output = self.pos_ffn(dec_output)
         return dec_output, dec_slf_attn, dec_enc_attn
